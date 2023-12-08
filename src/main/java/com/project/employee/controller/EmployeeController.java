60345cf7a56a4520f3425846d4594185a0de16d4
yusufbgdd557 <yusufbgdd557@gmail.com>
2023-12-09 02:08:34 +0300

    CORS Policy configuration

--- src/main/java/com/project/employee/controller/EmployeeController.java
+++ src/main/java/com/project/employee/controller/EmployeeController.java
@@ -4,6 +4,7 @@ import com.project.employee.model.Employee;
 import com.project.employee.repository.EmployeeRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:3000")
 @AllArgsConstructor
 @RequestMapping("/api/v1/")
 public class EmployeeController {
